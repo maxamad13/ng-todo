fe3fed170039fc00f5a0f1f8a92659cb151c0fd1
Mohamed Sharif <mohame@gmail.com>
2015-11-09 19:45:13 -0600

    second comm

--- main.js
+++ main.js
@@ -2,4 +2,11 @@ var app = angular.module('TodoApp',[])
 
 app.controller("TodoCtrl", function($scope){
 	$scope.title="Welcome to your Todo App"
+	$rootScope.hello="i am weverywhere"
+
+})
+
+app.controller("PastaCtrl", function($scope)
+	$scope.pasta= "Ilove pasta"
+	$scope.
 })
